dd8bfd446607f3561dee3dad5e7d31fe7baa8ef1
Juansin <joaquinn1284@hotmail.com>
2026-05-20 16:17:41 -0300

    pipipip

--- 20mayo.py
+++ 20mayo.py
@@ -8,9 +8,23 @@ def crear_mascota(nombre, raza, edad, energia=50):
 def alimentar_mascota(mascota, cantidad):
     mascota['energia'] += cantidad
 
+def jugar(mascota, tiempo):
+    mascota['energia'] -= tiempo * 5 
+
 
 dogui=crear_mascota("pelusa","Labrador", 4)
 pirulo=crear_mascota("pirulo","caniche",3, 70)
 print(dogui)
 print(pirulo)
 
+alimentar_mascota(dogui, 20)
+alimentar_mascota(pirulo, 39)
+
+print(dogui)
+print(pirulo)
+
+jugar(dogui, 100)
+jugar(pirulo, 3)
+
+print(dogui)
+print(pirulo)
