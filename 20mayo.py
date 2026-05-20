4a3774c833966d1b3fa95a1d8ac31ac4727fa3be
Juansin <joaquinn1284@hotmail.com>
2026-05-20 16:05:10 -0300

    aaaa

--- 20mayo.py
+++ 20mayo.py
@@ -1,9 +1,16 @@
-def crear_mascota(nombre, raza, edad, energia=50)
+def crear_mascota(nombre, raza, edad, energia=50):
     return {
         "nombre" : nombre,
         "raza" : raza,
         "edad" : edad,
         "energia" : energia
     }
+def alimentar_mascota(mascota, cantidad):
+    mascota['energia'] += cantidad
+
+
 dogui=crear_mascota("pelusa","Labrador", 4)
-pirulo=crear_mascota("pirulo","caniche",3)
+pirulo=crear_mascota("pirulo","caniche",3, 70)
+print(dogui)
+print(pirulo)
+
